2c5271071c0a2457154068ba3804150713080222
Jacob Grahn <contact@jiggmin.com>
2014-05-04 00:32:12 -0400

    don't crash if sending a report to globe doesn't return json

--- config/env.example.js
+++ config/env.example.js
@@ -4,5 +4,5 @@ process.env.APP_NAME = 'futurism-multi';
 process.env.MONGO_URI = '';
 process.env.REDIS_URI = '';
 
-process.env.GLOBE_URI = 'http://localhost:9001';
+process.env.GLOBE_URI = 'http://localhost:9002';
 process.env.GLOBE_KEY = 'secret';
